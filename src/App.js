4b4e1bdf7c80102aa2a8768fc252f5d0a45ca424
fabricio <fabricio.bizotto@ifc.edu.br>
2022-03-26 11:58:03 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageGallery from 'react-image-gallery'
+
+function importarImagens(params) {
+    let images = [];
+    params.keys().map((item, index) => { 
+      images.push({
+        original: process.env.PUBLIC_URL + "/img/" + item.replace('./', ''),
+        thumbnail: process.env.PUBLIC_URL + "/img/" + item.replace('./', ''),
+      });
+    });
+    return images;
+}
 
 function App() {
+  const images = importarImagens(require.context('../public/img/', false, /\.(png|jpe?g)$/));
+  // console.log(images);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ImageGallery items={images} thumbnailPosition="left" />
     </div>
   );
 }
